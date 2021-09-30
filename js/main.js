9945c5a68eb1e76c203b2a09091f0a41e38c6f11
Daniel <itzdanielmichael@gmail.com>
2021-09-30 14:02:42 -0400

    update main.js add mobile device detector

--- js/main.js
+++ js/main.js
@@ -5,13 +5,34 @@ var G;
 var controller;
 
 window.onload = function () {
-  var config = {
-    type: Phaser.AUTO,
-    width: 480,
-    height: 640,
-    parent: 'div-tag-name',
-    scene: [SceneMain], // game screens for the game
-  };
+  // detect mobile device
+  // if text includes Mobile
+  var isMobile = navigator.userAgent.includes('Mobile');
+  if (!isMobile) {
+    // else return if includes Tablet
+    isMobile = navigator.userAgent.includes('Tablet');
+  }
+
+  if (!isMobile) {
+    // if isn't mobile device
+    var config = {
+      type: Phaser.AUTO,
+      width: 480,
+      height: 640,
+      parent: 'div-tag-name',
+      scene: [SceneMain], // game screens for the game
+    };
+  } else {
+    // if is mobile device
+    var config = {
+      type: Phaser.AUTO,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      parent: 'phaser-game',
+      scene: [SceneMain],
+    };
+  }
+
   G = new Constants();
   model = new Model();
   game = new Phaser.Game(config);
