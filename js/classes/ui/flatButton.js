d00b50b0db871376a84fc5012adafd3d12efa944
Daniel <itzdanielmichael@gmail.com>
2021-10-07 23:09:08 -0400

    add flatButton

---
+++ js/classes/ui/flatButton.js
@@ -0,0 +1,37 @@
+class FlatButton extends Phaser.GameObjects.Container {
+  // config = props
+  constructor(config) {
+    if (!config.scene) {
+      console.error('missing scene!');
+      return;
+    }
+
+    if (!config.key) {
+      console.error('missing key!');
+      return;
+    }
+
+    super(config.scene);
+
+    this.scene = config.scene;
+    this.background = this.scene.add.image(0, 0, config.key); // x ,y , key
+
+    this.add(this.background);
+
+    if (config.text) {
+      this.text1 = this.scene.add.text(0, 0, config.text);
+      this.text1.setOrigin(0.5, 0.5);
+      this.add(this.text1);
+    }
+
+    if (config.x) {
+      this.x = config.x;
+    }
+
+    if (config.y) {
+      this.y = config.y;
+    }
+
+    this.scene.add.existing(this);
+  }
+}
--- js/sceneMain.js
+++ js/sceneMain.js
@@ -4,29 +4,34 @@ class SceneMain extends Phaser.Scene {
   }
   preload() {
     // load our images or sounds
-    this.load.image('face', 'images/face.png');
+    // this.load.image('face', 'images/face.png');
+    this.load.image('button1', 'images/ui/buttons/flat/1.png');
   }
   create() {
     // define our objects
-    emitter = new Phaser.Events.EventEmitter();
+    // emitter = new Phaser.Events.EventEmitter();
+    // // model.score = 100;
+    // controller = new Controller();
+    // // this.scoreBox = new ScoreBox({ scene: this });
+    // // this.scoreBox.x = game.config.width / 2;
+    // // this.scoreBox.y = 50; // 50px down from y 0
+    // // numbered grid system
+    // let gridConfig = { rows: 5, cols: 5, scene: this };
+    // let alignGrid = new AlignGrid(gridConfig);
+    // // alignGrid.show(); // shows the grid and cells with no numbers
+    // alignGrid.showNumbers(); // shows the grid and cells with numbers for placeAtIndex.
+    // this.face = this.add.sprite(0, 0, 'face');
+    // // alignGrid.placeAt(2, 2, this.face); // center
+    // alignGrid.placeAtIndex(16, this.face);
+    // Align.scaleToGameWidth(this.face, 0.2); // obj, percent
 
-    // model.score = 100;
-    controller = new Controller();
-
-    // this.scoreBox = new ScoreBox({ scene: this });
-    // this.scoreBox.x = game.config.width / 2;
-    // this.scoreBox.y = 50; // 50px down from y 0
-
-    // numbered grid system
-    let gridConfig = { rows: 5, cols: 5, scene: this };
-    let alignGrid = new AlignGrid(gridConfig);
-    // alignGrid.show(); // shows the grid and cells with no numbers
-    alignGrid.showNumbers(); // shows the grid and cells with numbers for placeAtIndex.
-
-    this.face = this.add.sprite(0, 0, 'face');
-    // alignGrid.placeAt(2, 2, this.face); // center
-    alignGrid.placeAtIndex(16, this.face);
-    Align.scaleToGameWidth(this.face, 0.2); // obj, percent
+    let flatButton = new FlatButton({
+      scene: this,
+      key: 'button1',
+      text: 'Press ME!',
+      x: 100,
+      y: 100,
+    });
   }
   update() {
     // constant running loop
